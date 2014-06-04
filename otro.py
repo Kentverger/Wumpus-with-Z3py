c03ea0eac6c059980942139ba282bb22249d2bb5
Luis Carlos Gonzalez Hernandez <lcarlosglez@gmail.com>
2014-06-04 13:34:13 -0500

    Reglas completas

--- otro.py
+++ otro.py
@@ -10,6 +10,8 @@ PozoFunction = Function('PozoFunction', IntSort(), IntSort(), BoolSort())
 HedorFunction = Function('HedorFunction', IntSort(), IntSort(), BoolSort())
 WumpusFunction = Function('WumpusFunction', IntSort(), IntSort(), BoolSort())
 SeguraFunction = Function('SeguraFunction', IntSort(), IntSort(), BoolSort())
+ResplandorFunction = Function('ResplandorFunction', IntSort(), IntSort(), BoolSort())
+OroFunction = Function('OroFunction', IntSort(), IntSort(), BoolSort())
 
 #Para todo x y que tenga brisa implica que hay un pozo en los adyacentes
 pozo = ForAll( [z3_x, z3_y], Implies( BrisaFunction( z3_x, z3_y ), Or( PozoFunction( z3_x + 1, z3_y ), PozoFunction( z3_x - 1, z3_y ), PozoFunction( z3_x, z3_y + 1 ), PozoFunction( z3_x, z3_y - 1 ) ) ) )
@@ -18,15 +20,21 @@ pozo = ForAll( [z3_x, z3_y], Implies( BrisaFunction( z3_x, z3_y ), Or( PozoFunct
 hedor = ForAll( [z3_x, z3_y], Implies( HedorFunction( z3_x, z3_y ), Or( WumpusFunction( z3_x + 1, z3_y ), WumpusFunction( z3_x - 1, z3_y ), WumpusFunction( z3_x, z3_y + 1 ), WumpusFunction( z3_x, z3_y - 1 ) ) ) )
 #no_hedor = ForAll( [z3_x, z3_y], Implies( Not(HedorFunction( z3_x, z3_y )), And( Not(WumpusFunction( z3_x + 1, z3_y )), Not(WumpusFunction( z3_x - 1, z3_y )), Not(WumpusFunction( z3_x, z3_y + 1 )), Not(WumpusFunction( z3_x, z3_y - 1 ) ) ) ) )
 
-reglas = And(pozo, hedor)
+casilla_segura = ForAll([z3_x, z3_y], Implies( And( Not( BrisaFunction( z3_x, z3_y ) ), Not( HedorFunction( z3_x, z3_y ) ), Not( PozoFunction( z3_x, z3_y ) ), Not( WumpusFunction( z3_x, z3_y ) ) ), SeguraFunction( z3_x, z3_y ) ) )
+
+casilla_con_oro = ForAll([z3_x, z3_y], Implies(ResplandorFunction(z3_x, z3_y), OroFunction(z3_x, z3_y) ) )
+
+reglas = And(pozo, hedor, casilla_segura, casilla_con_oro)
 
 persepciones_de_la_casilla = And( Not( BrisaFunction( 1, 1 ) ), Not( HedorFunction( 1, 1 ) ), Not( PozoFunction( 1, 1 ) ), Not( WumpusFunction( 1, 1 ) ) )
 
-coso = Implies( reglas, persepciones_de_la_casilla ) 
+reglas = And( reglas, persepciones_de_la_casilla )
+
+coso = Implies( reglas, SeguraFunction( 2 , 1) ) 
 
 prove(coso)
 
-reglas = And( reglas, persepciones_de_la_casilla )
+
 
 '''s = Solver()
 s.add(coso)
